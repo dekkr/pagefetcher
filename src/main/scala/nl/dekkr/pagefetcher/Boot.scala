1a49d3e8669f3e5e28dd2dea8c3a747e3f4ea09d
Matthijs <matthijs@plamola.com>
2015-04-17 17:31:46 +0200

    #5: moved settings to application.conf

--- src/main/scala/nl/dekkr/pagefetcher/Boot.scala
+++ src/main/scala/nl/dekkr/pagefetcher/Boot.scala
@@ -2,6 +2,7 @@ package nl.dekkr.pagefetcher
 
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
+import com.typesafe.config.ConfigFactory
 import spray.can.Http
 import akka.pattern.ask
 import akka.util.Timeout
@@ -15,7 +16,9 @@ object Boot extends App {
   // create and start our service actor
   val service = system.actorOf(Props[FrontendServiceActor], "page-fetch-service")
 
-  implicit val timeout = Timeout(5.seconds)  //TODO move timeout to config [Issue #5]
+  implicit val timeout = Timeout(ConfigFactory.load().getInt("nl.dekkr.pagefetcher.api.timeout").seconds)
   // start a new HTTP server on port 8080 with our service actor as the handler
-  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)    //TODO move interface & port to config [Issue #5]
+  IO(Http) ? Http.Bind(service,
+    interface = ConfigFactory.load().getString("nl.dekkr.pagefetcher.api.host"),
+    port = ConfigFactory.load().getInt("nl.dekkr.pagefetcher.api.port"))
 }
